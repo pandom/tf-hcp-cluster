98c62fc321a81adff2d25592de79085e85c490dc
pandom <pandom@users.noreply.github.com>
2021-11-16 10:25:10 +1100

    fixes auth

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 // Configure the provider
 provider "hcp" {
     client_id = var.hcp_client_id
-    client_secret =  = var.hcp_secret_id
+    client_secret =  var.hcp_secret_id
 }
 
 // Use the cloud provider AWS to provision resources that will be connected to HCP
