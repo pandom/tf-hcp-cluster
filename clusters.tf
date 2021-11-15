4869a91ff78a9019708568da1b0bf2f4f6cc0e0b
pandom <pandom@users.noreply.github.com>
2021-11-16 10:19:41 +1100

    tidy descriptions

--- clusters.tf
+++ clusters.tf
@@ -1,4 +1,4 @@
-// Create an HVN
+// Create an HVN for Vault
 resource "hcp_hvn" "vault_hvn" {
   hvn_id         = "hcp-tf-vault-hvn"
   cloud_provider = "aws"
@@ -6,14 +6,14 @@ resource "hcp_hvn" "vault_hvn" {
   cidr_block     = "172.25.16.0/20"
 }
 
-// Create a peering connection between two HVNs
+## Create HVN for Consul
 resource "hcp_hvn" "consul_hvn" {
   hvn_id         = "hcp-tf-consul-hvn"
   cloud_provider = "aws"
   region         = var.region
   cidr_block     = "172.18.16.0/20"
 }
-
+// Create a peering connection between two HVNs
 resource "hcp_hvn_peering_connection" "peer" {
   hvn_1 = hcp_hvn.vault_hvn.self_link
   hvn_2 = hcp_hvn.consul_hvn.self_link
