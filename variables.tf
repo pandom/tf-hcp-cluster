61630a845e9cdd4ccef788bfe7a75a55a791fe9e
pandom <pandom@users.noreply.github.com>
2022-03-03 11:58:00 +1100

    adds declaration.

--- variables.tf
+++ variables.tf
@@ -3,12 +3,12 @@ variable "region" {
     default = "ap-southeast-2"  
 }
 
-# variable "hcp_client_id" {
-#     description = "hcp-client-id"
-#     type = string
-# }
+variable "hcp_client_id" {
+    description = "hcp-client-id"
+    type = string
+}
 
-# variable "hcp_secret_id" {
-#     description = "secret"
-#     type = string
-# }
+variable "hcp_secret_id" {
+    description = "secret"
+    type = string
+}
