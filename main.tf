feeba49496b87cec0451e5ec971135c3bedd0b87
pandom <pandom@users.noreply.github.com>
2022-03-08 13:10:18 +1100

    client secret!

--- main.tf
+++ main.tf
@@ -4,16 +4,24 @@ terraform {
       source  = "hashicorp/hcp"
       version = "0.22.0"
     }
+    vault = {
+      source = "hashicorp/vault"
+      version = "3.3.1"
+    }
   }
 }
 
 // Configure the provider
 provider "hcp" {
     client_id = var.hcp_client_id
-    client_secret =  var.hcp_secret_id
+    client_secret =  var.hcp_client_secret
 }
 
 // Use the cloud provider AWS to provision resources that will be connected to HCP
 provider "aws" {
   region = var.region
+}
+
+provider "vault" {
+  
 }
