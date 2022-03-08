ac7780e0eb8bda3c75947d2c8a58e22cda580bae
pandom <pandom@users.noreply.github.com>
2022-03-08 13:08:34 +1100

    Defaults

--- variables.tf
+++ variables.tf
@@ -6,11 +6,13 @@ variable "region" {
 variable "hcp_client_id" {
     description = "hcp-client-id"
     type = string
+    default = ""
 }
 
 variable "hcp_client_secret" {
     description = "secret"
     type = string
+    default = ""
 }
 
 variable "root_policy" {
