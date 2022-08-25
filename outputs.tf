b85a09019e293629747561c894726310d30006ee
pandom <pandom@users.noreply.github.com>
2022-08-25 13:34:22 +1000

    No vault token output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "vault_token" {
-  sensitive = false
-  value = hcp_vault_cluster_admin_token.token.token
-}
+# output "vault_token" {
+#   sensitive = true
+#   value = hcp_vault_cluster_admin_token.token.token
+# }
 
 output "public_address" {
     value = hcp_vault_cluster.vault.vault_private_endpoint_url
